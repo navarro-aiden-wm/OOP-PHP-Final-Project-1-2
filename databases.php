083cd5c4555a3a50249348e11e897e62d3fda757
Aiden Navarro <aiden.navarro@west-mec.org>
2016-09-27 18:06:14 -0700

    project initial commit

---
+++ databases.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: session2
+ * Date: 9/27/16
+ * Time: 5:07 PM
+ */
+
+class BlogPost{
+    public $Id;
+    public $Title;
+    public $Post;
+    public $AuthorId;
+    public $DatePosted;
+
+    function __construct($inId=null, $inTitle=null, $inPost=null,  $inAuthorId=null, $inDatePosted=null){
+
+    }
+}
---
+++ index1.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: session2
+ * Date: 9/27/16
+ * Time: 5:08 PM
+ */
+
+require 'databases.php';
